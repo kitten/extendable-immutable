88cbc7b293d811d952f309c0bc093f095e475275
Phil Plückthun <phil@piemapping.com>
2016-08-01 18:39:07 +0100

    Add exports to index.js

--- src/extendable.js
+++
@@ -1,4 +0,0 @@
-import invariant from 'invariant'
-import createExtendable from './util/createExtendable'
-
-function
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import createExtendable from './util/createExtendable'
 import Map from './Map'
 import List from './List'
+import OrderedMap from './OrderedMap'
+import Set from './Set'
 
 export {
   Map,
   List,
+  Set,
+  OrderedMap,
 
   createExtendable
 }
 
 export default {
   Map,
-  List
+  List,
+  Set,
+  OrderedMap
 }
 
